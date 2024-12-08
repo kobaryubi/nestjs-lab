d971765c153a322a23ef73db37a9e169a784cc50
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-12-08 00:00:24 +0000

    feat: fastify

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 const bootstrap = async () => {
-  const app = await NestFactory.create()
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
   await app.listen(3000)
 }
 
